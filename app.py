f93aa01c7b11ca824965f768daca7d2a7824d206
Zach Snyder <sadin@fedoraproject.org>
2022-02-17 17:08:20 -0500

    basic price checking added for USD given a coin shortcode

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 import os,discord,requests,string
+from coinbase.wallet.client import Client as CoinbaseClient
 from getpass import getpass
 
 bot_token = os.environ.get('CRYPPY_TOKEN')
+api_key = os.environ.get('API_KEY')
+api_secret = os.environ.get('COINBASE_SECRET')
 
 if not bot_token:
     print('No env variable CRYPPY_TOKEN set..\n')
     bot_token = getpass('token: ')
 
 client = discord.Client()
+coinbase_client = CoinbaseClient(api_key, api_secret, api_version='2021-02-07')
 
 @client.event
 async def on_ready():
@@ -32,16 +36,22 @@ async def on_message(message):
             return
 
 async def price_check(message, message_body):
+    # check coinbase for price of coin specified
+    # handle empty message after command
     if not message_body == '':
-        # check coinbase for price of coin specified
         response = f'price check request recieved for {message_body}'
     else:
         response = 'price check request recieved but no coin specified'
-        
+
+    print(response)
+    await message.channel.send(response)
+
+    price = coinbase_client.get_spot_price(currency_pair=f'{str.upper(message_body)}-USD')
+    response = f'price per {str.upper(message_body)} is {price["amount"]} USD'
+
     print(response)
     await message.channel.send(response)
     
-
 
 
 client.run(bot_token)
