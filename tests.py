913570e3b1dfa5bb6e4a2b8f3eabe67b83eff388
Zach Snyder <sadin@fedoraproject.org>
2022-02-21 23:06:53 -0500

    more tests added before writing parse_currency_pair func

--- tests.py
+++ tests.py
@@ -9,7 +9,9 @@ from app.py import parse_currency_pair
 @pytest.mark.parametrize(
     ('input_string', 'expected'),
     (
-        ('ETH USD', ('ETH', 'USD'))
+        ('ETH USD', ('ETH', 'USD')),
+        ('ETH EUR', ('ETH', 'EUR')),
+        ('ETH USD Additional text for edge case', ('ETH', 'USD'))
     )
 )
 def test_parse_currency_pair(input_string: str, expected: tuple):
