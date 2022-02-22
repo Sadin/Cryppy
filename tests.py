9d0ecc14c6627c0d63982fa92459ed39944eb98b
Zach Snyder <sadin@fedoraproject.org>
2022-02-21 23:03:22 -0500

    parse currency pair test

--- tests.py
+++ tests.py
@@ -1,2 +1,16 @@
 # test cases for Cryppy
 from __future__ import annotations
+
+import pytest
+
+from app.py import parse_currency_pair
+
+
+@pytest.mark.parametrize(
+    ('input_string', 'expected'),
+    (
+        ('ETH USD', ('ETH', 'USD'))
+    )
+)
+def test_parse_currency_pair(input_string: str, expected: tuple):
+    assert parse_currency_pair(input_string) == expected
