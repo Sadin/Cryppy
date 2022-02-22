f25951fa172c7aa2319bcbfe500ff56bc82586ce
Zach Snyder <sadin@fedoraproject.org>
2022-02-22 00:26:33 -0500

    parse_currency_pair function complete passing tests

--- cryppy/helpers.py
+++ cryppy/helpers.py
@@ -2,4 +2,9 @@ from __future__ import annotations
 
 
 def parse_currency_pair(input_string: str) -> tuple:
-    return tuple(input_string.split(' ', 1))
+    currency_pair = input_string.strip()
+    currency_pair = currency_pair.split(' ', maxsplit=2)
+    if len(currency_pair) > 2:
+        currency_pair.pop()
+
+    return tuple(currency_pair)
